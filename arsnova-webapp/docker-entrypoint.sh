eb4ae5aa10a34e75b04345932801082b0384996d
Daniel Gerhardt <code@dgerhardt.net>
2017-04-04 12:03:02 +0200

    Allow configuration of root-url setting
    A new arsnova.env file for environment variables has been added which is
    currently used to modify settings in arsnova.properties with sed at
    startup.

--- arsnova-webapp/docker-entrypoint.sh
+++ arsnova-webapp/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /arsnova-setuptool-"$ARSNOVA_SETUPTOOL_VERSION" && python tool.py
+sed -i "s@^root-url=.*@root-url=$ROOT_URL@g" /etc/arsnova/arsnova.properties
 export CATALINA_HOME=/usr/share/tomcat8
 export CATALINA_BASE=/var/lib/tomcat8
 $CATALINA_HOME/bin/catalina.sh $*
