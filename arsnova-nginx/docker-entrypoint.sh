42e8ece38a81c8aeb44585df0ec9e3362576e1f4
Daniel Gerhardt <daniel.gerhardt@mni.thm.de>
2017-04-06 15:11:47 +0200

    Improve startup scripts
    Services now test if their dependencies are up before continuing with
    startup. "$@" is used instead of $* to pass command-line arguments to
    Tomcat.

---
+++ arsnova-nginx/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+  printf "Testing upstream connection"
+  for i in `seq 1 10`; do
+    printf .
+    curl -o /dev/null -s -I -f "http://webapp:8080/api/"
+    if [ $? -eq 0 ]; then
+      echo " OK."
+      break;
+    elif [ $i -ge 10 ]; then
+      echo " FAILED."
+      exit 1;
+    fi
+    sleep 10;
+  done
+  nginx -g "daemon off;"
+fi
+"$@";
--- arsnova-webapp/docker-entrypoint.sh
+++ arsnova-webapp/docker-entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
-cd /arsnova-setuptool-"$ARSNOVA_SETUPTOOL_VERSION" && python tool.py
-sed -i "s@^root-url=.*@root-url=$ROOT_URL@g" /etc/arsnova/arsnova.properties
+if [ "$1" = run ]; then
+  printf "Testing database connection"
+  for i in `seq 1 10`; do
+    printf .
+    curl -o /dev/null -s -I -f "http://couchdb:5984/"
+    if [ $? -eq 0 ]; then
+      echo " OK."
+      break;
+    elif [ $i -ge 10 ]; then
+      echo " FAILED."
+      exit 1;
+    fi
+    sleep 10;
+  done
+  cd /arsnova-setuptool-"$ARSNOVA_SETUPTOOL_VERSION" && python tool.py
+  sed -i "s@^root-url=.*@root-url=$ROOT_URL@g" /etc/arsnova/arsnova.properties
+fi
 export CATALINA_HOME=/usr/share/tomcat8
 export CATALINA_BASE=/var/lib/tomcat8
-$CATALINA_HOME/bin/catalina.sh $*
+$CATALINA_HOME/bin/catalina.sh "$@"
